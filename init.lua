bab7c687a544d3bfc3a3ca2a1465a4f03f6a0802
Vimal Mollyn <ms123vimal@gmail.com>
2025-12-14 22:23:10 -0800

    bookmarks.yazi works again, upgraded to yazi 25.5.31

--- init.lua
+++ init.lua
@@ -16,12 +16,32 @@ require("full-border"):setup {
 	type = ui.Border.ROUNDED,
 }
 
+-- require("bookmarks"):setup({
+-- 	persist = "all",
+-- 	desc_format = "full",
+-- 	last_directory = { enable = true, persist = true },
+--     show_keys= true,
+-- 	notify = {
+-- 		enable = true,
+-- 		timeout = 1,
+-- 		message = {
+-- 			new = "New bookmark '<key>' -> '<folder>'",
+-- 			delete = "Deleted bookmark in '<key>'",
+-- 			delete_all = "Deleted all bookmarks",
+-- 		},
+-- 	},
+-- })
+
+-- ~/.config/yazi/init.lua
 require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode="dir" },
 	persist = "all",
 	desc_format = "full",
-	last_directory = { enable = true, persist = true },
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	show_keys = false,
 	notify = {
-		enable = true,
+		enable = false,
 		timeout = 1,
 		message = {
 			new = "New bookmark '<key>' -> '<folder>'",
